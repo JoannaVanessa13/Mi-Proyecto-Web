472c29730c33e9c2e6ad442bda7e3b71231f22a3
JoannaVanessa13 <faa61566@gmail.com>
2022-07-17 23:04:32 -0500

    commit 23

--- json/API_poke.js
+++ json/API_poke.js
@@ -1,7 +1,7 @@
 var idp=1;
 var long=0;
 
-    const url = "https://joannavanessa13.github.io/Ejem_json_api/";
+    const url = "https://joannavanessa13.github.io/Ejem_json_api/poke.json";
 
     function buscar(){
     
@@ -11,28 +11,28 @@ var long=0;
         })
     
         .then(function(data){
-            console.log(data.figures);
-            long=data.figures.length;
+            console.log(data.personajes);
+            long=data.personajes.length;
             console.log("lenght:"+ long);
         
-            var res= data.figures.filter(item=>item.id===idp);
+            var res= data.personajes.filter(item=>item.id===idp);
             console.log(res);
             var imagen=document.getElementById('pkm-img');
             imagen.src=res[0].figure;
             var p1=document.getElementById('name');
-            p1.innerHTML="<b>Nombre: </b>"+res[0].name;
+            p1.innerHTML="<b>Nombre: </b>"+res[0].nombre;
             var p2=document.getElementById('type');
-            p2.innerHTML="<b>Tipo: </b>"+res[0].type;
+            p2.innerHTML="<b>Tipo: </b>"+res[0].tipo;
             var p3=document.getElementById('height');
-            p3.innerHTML="<b>Altura: </b>"+res[0].height+"cms.";
+            p3.innerHTML="<b>Altura: </b>"+res[0].altura+"cms.";
             var p4=document.getElementById('weight');
-            p4.innerHTML="<b>Peso: </b>"+res[0].weight+"kg.";
+            p4.innerHTML="<b>Peso: </b>"+res[0].peso+"kg.";
             var p5=document.getElementById('des');
-            p5.innerHTML="<b>Descripción: </b>"+res[0].des;
+            p5.innerHTML="<b>Descripción: </b>"+res[0].descripcion;
         })
 
         .catch(function(error){
-            console.log(error);           
+            console.log();           
         });
 
         if (idp >= long){
