1c5df0b7ba90b864b161469491bb9cc6a0df8691
JoannaVanessa13 <faa61566@gmail.com>
2022-08-05 18:58:48 -0500

    commit 50

--- json/API_musica.js
+++ json/API_musica.js
@@ -1,7 +1,7 @@
 var idp=1
 var long=0
         
-    const url = "https://joannavanessa13.github.io/Ejem_json_api/musica.json ";
+    const url = " https://joannavanessa13.github.io/Ejem_json_api/musica.json ";
 
     /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
     function myFunction() {
